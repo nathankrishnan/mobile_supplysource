f4db6e15b29193d772b78bab6c734ed0e5304897
nathankrishnan <nathan.a.krishnan@gmail.com>
2013-10-27 01:31:13 -0400

    Fixed UI width in Android :)

--- www/javascripts/application.js
+++ www/javascripts/application.js
@@ -1,1 +1,17 @@
 steroids.view.navigationBar.show("Supply Source");
+
+var rightButton = new steroids.buttons.NavigationBarButton();
+
+rightButton.title = "Scan"
+rightButton.onTap = function() { alert("Right button tapped"); }
+
+steroids.view.navigationBar.setButtons({
+  right: [rightButton]
+}, {
+  onSuccess: function() {
+    alert("Buttons set!");
+  },
+  onFailure: function() {
+    alert("Failed to set buttons.");
+  }
+});
